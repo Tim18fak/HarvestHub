6fa7915921e98dd2284a27e66af0565858eae041
Timothy Avell <timothy.avell.olatunde@gmail.com>
2023-10-25 00:09:59 -0400

    Product Details stored in database and a folder to store images of the product

--- server/Routes/farmerUser.js
+++ server/Routes/farmerUser.js
@@ -1,7 +1,7 @@
 const express = require('express')
-
+const {createProduct} = require('../Controller/Farmers/product')
 
 const router = express.Router()
 
-
+router.post('/createProduct',createProduct)
 module.exports = router
--- server/server.js
+++ server/server.js
@@ -4,8 +4,17 @@ const fs = require('fs');
 const express = require('express');
 const cors = require('cors');
 
+
+
+const chat = require('./Routes/Chat')
+const authRoutes = require('./Routes/auth.js')
+const farmerRoutes = require('./Routes/farmerUser')
+
+
+
+
 const corsOptions = {
-  origin: "*",
+  origin: "http://localhost:5174",
 };
 
 
@@ -13,8 +22,7 @@ const corsOptions = {
 const app = express();
 app.use(express.json()); // this is important to be able eto send info from the client to my server
 // Routes
-const chat = require('./Routes/Chat')
-const authRoutes = require('./Routes/auth.js')
+
 const options = {
   key: fs.readFileSync('key.pem'),
   cert: fs.readFileSync('cert.pem'),
@@ -27,6 +35,13 @@ server.listen(443, () => {
   console.log('Server is running on https://localhost');
 });
 // route links
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5174");
+  // Other CORS headers and configurations can be added here
+  next();
+});
+app.use("/images", express.static("uploads"));
+app.use('/farmerUser',farmerRoutes)
 app.use(cors(corsOptions));
 app.use('/auth', authRoutes)
 app.use('/chat', chat)
