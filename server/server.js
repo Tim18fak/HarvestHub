41003640c30da915facc2ba9bc7f68747d28674b
Tim18fak <timothy.avell.olatunde@gmail.com>
2023-11-06 08:15:00 -0500

    http configuration

--- server/server.js
+++ server/server.js
@@ -1,45 +1,55 @@
+const http = require('http'); // Change 'https' to 'http'
 const Mongodb = require('./mongodb');
+const fs = require('fs');
 const express = require('express');
 const cors = require('cors');
 const { Server } = require('socket.io');
-
-/* const chat = require('./Routes/Chat'); */
 const authRoutes = require('./Routes/auth.js');
 const farmerRoutes = require('./Routes/farmerUser');
 
 const corsOptions = {
-  origin: '*', // Replace with the origin of your frontend
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify the allowed HTTP methods
-  allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization', // Specify allowed headers
+  origin: "*",
 };
 
 const app = express();
-app.use(express.json());
-// Use the cors middleware with your options
-app.use(cors());
+app.use(express.json()); // This is important to be able to send info from the client to my server
 
-const server = app.listen(80, () => {
-  console.log('Server is running on http://localhost');
-});
+// Routes
+const chat = require('./Routes/Chat');
+
+const server = http.createServer(app); // Change 'https' to 'http'
 
 const io = new Server(server, {
   cors: {
     origin: '*', // Allow requests from your React app's domain
     methods: ['GET', 'POST'],
   },
-});
+}); // Create a Socket.IO server
 
+// Define a socket.io connection event
 io.on('connection', (socket) => {
   console.log('A user connected' + socket.id);
 });
 
-// Add your routes and endpoints here
+server.listen(80, () => { // Change the port number to 80 for HTTP
+  console.log('Server is running on http://localhost');
+});
+
+// Route links
+app.use((req, res, next) => {
+  req.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  // Other CORS headers and configurations can be added here
+  next();
+});
+
+app.use(cors(corsOptions));
 app.use("/productimages", express.static("uploads"));
 app.use("/profileimages", express.static("ProfileImages"));
 app.use('/farmerUser', farmerRoutes);
+app.use(cors(corsOptions));
 app.use('/auth', authRoutes);
-/* app.use('/chat', chat); */
+app.use('/chat', chat);
 
 app.get('/', (req, res) => {
-  res.send('Hello, HTTP World!');
+  res.send('Hello, HTTP World!'); // Update the response message
 });
