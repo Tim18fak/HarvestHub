b9f67eccf856fe3a44b7fba6a50ddd66f0b95bea
Tim18fak <timothy.avell.olatunde@gmail.com>
2023-11-06 09:20:57 -0500

    bug fixing

--- server/server.js
+++ server/server.js
@@ -1,32 +1,23 @@
-const https = require('https');
-const Mongodb = require('./mongodb')
+const http = require('http'); // Change 'https' to 'http'
+const Mongodb = require('./mongodb');
 const fs = require('fs');
 const express = require('express');
 const cors = require('cors');
-const { Server} = require('socket.io')
-
-
-/* const chat = require('./Routes/Chat') */
-const authRoutes = require('./Routes/auth.js')
-const farmerRoutes = require('./Routes/farmerUser')
-const admin = require('./Routes/admin')
+const { Server } = require('socket.io');
+const authRoutes = require('./Routes/auth.js');
+const farmerRoutes = require('./Routes/farmerUser');
 
 const corsOptions = {
   origin: "*",
 };
 
 const app = express();
-app.use(express.json()); // this is important to be able eto send info from the client to my server
-// Routes
-const chat = require('./Routes/Chat')
-/* const authRoutes = require('./Routes/auth.js') */
-const options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem'),
-  passphrase: 'jewel',
-};
+app.use(express.json()); // This is important to be able to send info from the client to my server
 
-const server = https.createServer(options, app);
+// Routes
+const chat = require('./Routes/Chat');
+
+const server = http.createServer(app); // Change 'https' to 'http'
 
 const io = new Server(server, {
   cors: {
@@ -39,24 +30,27 @@ const io = new Server(server, {
 io.on('connection', (socket) => {
   console.log('A user connected' + socket.id);
 });
-server.listen(443, () => {
-  console.log('Server is running on https://localhost');
+
+server.listen(80, () => { // Change the port number to 80 for HTTP
+  console.log('Server is running on http://localhost');
 });
-// route links
+
+// Route links
 app.use((req, res, next) => {
   req.header("Access-Control-Allow-Origin", "http://localhost:5173");
   // Other CORS headers and configurations can be added here
   next();
 });
+
 app.use(cors(corsOptions));
 app.use("/productimages", express.static("uploads"));
 app.use("/profileimages", express.static("ProfileImages"));
-app.use('/farmerUser',farmerRoutes)
+app.use('/farmerUser', farmerRoutes);
 app.use(cors(corsOptions));
-app.use('/auth', authRoutes)
-app.use('/chat', chat)
-app.use('/admin',admin)
+app.use('/auth', authRoutes);
+app.use('/chat', chat);
+
 app.get('/', (req, res) => {
-  res.send('Hello, HTTPS World!');
+  res.send('Hello, HTTP World!'); // Update the response message
 });
 
