109d1200466064b58dce1a44f452c4761658a310
Timothy Avell <timothy.avell.olatunde@gmail.com>
2023-11-18 08:07:59 -0500

    File structure Arrangement

--- server/tests/ActivationCode/code.test.js
+++ server/tests/ActivationCode/code.test.js
@@ -2,5 +2,5 @@ const axios = require('axios')
 
 test('Should be a Truthy ', async() => {
     const response = await axios.get('http://localhost/auth/code')
-    expect(response.data).toBeTruth()
+    expect(response.data).toBeTruthy()
 })
--- server/tests/Admin/admin_creation.test.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios')
-test('Admin exist', async() => {
-  const response = await axios.post('http://localhost/admin/admincreation',{
-    'username':'timothy',
-        'password': 'timothy',
-        'email':'timothy.avell.olatunde@gmail.com'
-  })
-  expect(response.data.message).toBe("Admin Account Already Exist")
-})
