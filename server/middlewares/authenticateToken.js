eec71ce8e30fcccbb0f9654b9a26b699a9ef8f83
Timothy Avell <timothy.avell.olatunde@gmail.com>
2023-11-19 23:41:04 -0500

    verification of Authorization token

--- server/middlewares/authenticateToken.js
+++ server/middlewares/authenticateToken.js
@@ -1,11 +1,15 @@
 const { jwt } = require("../config/jwt.config")
 
 const authenticateAdminToken = async(req,res,next) => {
-jwt.verify('htit',process.env.Admin_Authorization_Secret)
+jwt.verify('htit',process.env.Admin_Authorization_Secret,(err,data) =>{
+
+})
 
 }
 
 const authenticateClientToken = async(req,res,next) => {
-    jwt.verify('',process.env.Client_Authorization_Secret)
+    jwt.verify('',process.env.Client_Authorization_Secret,(err,data)=>{
+        
+    })
 }
 module.exports = {authenticateAdminToken,authenticateClientToken} 
