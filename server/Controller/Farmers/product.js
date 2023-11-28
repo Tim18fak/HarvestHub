af17f7231c38690597e4a741f983eeb9682642a7
Tim18fak <timothy.avell.olatunde@gmail.com>
2023-11-28 18:30:00 -0500

    Admin Logic

--- server/Controller/Farmers/product.js
+++ server/Controller/Farmers/product.js
@@ -2,57 +2,7 @@ const { Product, Farmer } = require('../../Model/DB_structure');
 const { Cloudinary } = require('../../config/cloudinary.config');
 const {ReqInfo} = require('./constants/reqInfo')
 const multer = require("multer");
-const { uploadImages } = require('./constants/uploadImages');
 
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/"); // Save uploaded files to the "uploads" directory
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname); // Use a unique name for the uploaded file
-  },
-});
-
-const upload = multer({ storage: storage });
-
-const createProduct = async (req, res) => {
-  try {
-    // Access the image and description using their respective field names
-    upload.fields([{ name: "image", maxCount: 5 }])(req, res, async (err) => {
-      if (err) {
-        return res.status(400).json({ error: "Image upload failed" });
-      }
-  
-      const {productImages,
-        description, 
-        quantity,
-        price, 
-        catergory,
-        location,
-        date,userid} = await ReqInfo(req);
-        
-      const newProduct = new Product({Image: productImages,
-        description: description, 
-        quantity: quantity,
-        price: price, 
-        catergory: catergory,
-        location: location,
-        date:date,
-      Farmer: userid})
-
-      newProduct.save()
-      .then(async (data) => {
-        const FarmerProductArray = Farmer.findById(userid)
-        FarmerProductArray.push(data._id)
-        await FarmerProductArray.save();
-      })
-      res.status(200).json({ message: "Product created successfully" });
-    });
-  } catch (error) {
-    res.status(500).send(error.message); // Respond with an error status
-  }
-};
 
 const deleteProduct = async(req,res) => {
   const {produceId} =  req.params
@@ -95,4 +45,4 @@ const testUploadProduct = async(req,res) => {
     console.log(error.message)
   }
 }
-module.exports = { createProduct, deleteProduct,testUploadProduct}
+module.exports = { deleteProduct,testUploadProduct}
