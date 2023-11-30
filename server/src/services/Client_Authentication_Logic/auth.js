a4a12b11c9e73af7e8044c41b2d7d2bb0eefae1a
Timothy Avell <timothy.avell.olatunde@gmail.com>
2023-11-29 20:07:51 -0500

    get Profile logic in progress

--- server/src/services/Client_Authentication_Logic/auth.js
+++ server/src/services/Client_Authentication_Logic/auth.js
@@ -180,6 +180,7 @@ const clientLogin = async(body,id,res) =>{
   }
   const {_id} =  clientLogin
   const accessToken = await clientAuthToken(_id,username,email,isFarmer)
+  console.log(_id,username,email,isFarmer)
   return res.status(200).json({accessToken,isFarmer,_id,username})
   }
   catch(err){
