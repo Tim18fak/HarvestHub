9cbf81b016f922e03666b6a5b0f263f450a789bd
Timothy Avell <timothy.avell.olatunde@gmail.com>
2023-11-29 22:40:50 -0500

    Consumer produce search logic

--- server/Controller/User/constant/productFilter.js
+++ server/Controller/User/constant/productFilter.js
@@ -1,8 +1,8 @@
 const productFilter = (resultSearch) => {
    const filteredResult = resultSearch.map((result) => {
-    const {_id,description,Image,date,quantity,price,Farmer} = result;
+    const {_id,description,Image,date,quantity,price,Farmer,title} = result;
     const {aboutYourself,fullname,phoneNumber,profileImage,farmName} = Farmer;
-    return {_id,description,Image,date,quantity,price,aboutYourself,fullname,phoneNumber,profileImage,farmName}
+    return {_id,description,Image,date,quantity,price,aboutYourself,fullname,phoneNumber,profileImage,farmName,title}
    })
       return filteredResult
 } 
