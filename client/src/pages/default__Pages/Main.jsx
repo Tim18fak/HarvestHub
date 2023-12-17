bed6a0b713db0c91438fe0ddbd1dd41e328dcf2f
KINI Biè Honoré <104803215+honokini@users.noreply.github.com>
2023-12-17 06:17:59 +0000

    Commit my Main jsx and css files

--- client/src/pages/default__Pages/Main.jsx
+++ client/src/pages/default__Pages/Main.jsx
@@ -1,13 +1,32 @@
+import './Main.css';
 
 const Main = () => {
   return (
     <>
-    <section>
-      
-
-    </section>
+      <section className="main-header" style={{height : (window.innerHeight - 83)}}>
+        <div className="row-1">
+          <span>Connecting Farmer's</span>
+          <button className="btn"></button>
+          <span>To Their Consumer's</span>
+        </div>
+        <div className="row-2">
+          <span>Through Modern Technology</span>
+        </div>
+        <div className="row-3">
+          <div className="col-1 col">
+            <div className="card-header"></div>
+          </div>
+          <div className="col-2 col">
+            <div className="card-header"></div>
+          </div>
+          <div className="col-3 col">
+            <div className="card-header"></div>
+          </div>
+        </div>
+      </section>
     </>
   )
 }
 
 export default Main
+
